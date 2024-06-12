0943540d15d053416c9f4531469a0409eb7709a2
4096autos <4096autos@gmail.com>
2024-06-12 08:45:37 +0000

    cours 3 | Types de donnees

--- MainApp.java
+++ MainApp.java
@@ -1,7 +1,23 @@
-class MainApp
+/*
+	bases numériques	: 	décimale		(10	-> 0 à 9) 	14
+							binéaire		(2	-> 0 à 1) 	0b11110111
+							octale			(8	-> 0 à 7) 	0755
+							hexadécimale	(16	-> 0 à F)	0xAF55
+	nombre entier 		: 	14
+							18_666
+							100_000_000_000
+
+	\n : nouvelle ligne
+	\t : tabulation
+	\r : retour chariot
+	\b : retour arrière
+	\f : nouvelle page
+
+*/
+public class MainApp
 {
 	public static void main(String[] args)
 	{
-		System.out.println("Bonjour tout le monde ! :)");
+		System.out.println("\tBonjour tout le monde ! :)\n");
 	}
 }
