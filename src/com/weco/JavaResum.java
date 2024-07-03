2dede391f9b9d96ffa179cbda512daed7906eeed
4096autos <4096autos@gmail.com>
2024-07-03 11:59:19 +0000

    Table / Tableau | Exercises on Table | ADD table.java

--- src/com/weco/JavaResum.java
+++ src/com/weco/JavaResum.java
@@ -276,7 +276,7 @@ NEW.Class - Constructeur - this - Attribut - OBJET/instance
 		}
 	}
 //-------------------------------------------------------------------------------------
-Encapsulation données - SoftwareRegistration - SoftwareRegistration.java
+//	Encapsulation données - SoftwareRegistration - SoftwareRegistration.java
 //-------------------------------------------------------------------------------------
 //	SoftwareRegistration.java [ACCESSEURS - setter and getter]
 public class SoftwareRegistration
@@ -301,4 +301,59 @@ public class SoftwareRegistration
     public void setExpirationYear(int expiration){ this.aExpirationYear = expiration;} //[ACCESSEURS : setter]
 
 	public static int getNumberOfRegistration(){ return aNumberOfRegistration;} //[ACCESSEURS : Getter]
+}
+//-------------------------------------------------------------------------------------
+//	Tableau - Table 1 & 2 Dimensions - Table.java
+//-------------------------------------------------------------------------------------
+//	Table.java [] |	* int tab[] / int[] * tab int tab[] = {1, 2, 3}	* int tab[] = new int [3]  * int tab[] = new int[]{1, 2, 3}
+
+package com.weco;
+
+public class Table {
+	/* * int tab[] / int[] tab int tab[] = {1, 2, 3} int tab[] = new int [3] int tab[] = new int[]{1, 2, 3} */
+
+	public Table() {}
+
+	public int aTable;
+
+	void printTab(int[] t) {
+		for (int element : t)
+			System.out.println(element);
+	}
+
+	void showTable() {
+		System.out.println("Tableau à 1 dimension");
+		int[] tab = { 1, 2, 3 };
+		System.out.println("Form 1");
+		System.out.println("-------");
+
+		for (int i = 0; i < tab.length; i++)
+			System.out.println(tab[i]);
+
+		System.out.println("Form 2");
+		System.out.println("-------");
+
+		for (int number : tab)
+			System.out.println(number);
+	}
+
+	void showTable2() {
+		System.out.println("Tableau à 2 dimension");
+		int[][] tab = { { 1, 2, 3 }, { 11, 11, 13 } };
+		System.out.println("Form 1");
+		System.out.println("-------");
+
+		for (int i = 0; i < tab.length; i++) {
+			for (int j = 0; j < tab[i].length; j++)
+				System.out.println(tab[i][j]);
+		}
+
+		System.out.println("Form 2");
+		System.out.println("-------");
+
+		for (int[] k : tab)
+			for (int number : k)
+				System.out.println(number);
+	}
+
 }
