96cc1954e2d8e2c5a96a616e0f19784a1a39a3dd
4096autos <4096autos@gmail.com>
2024-07-07 16:57:56 +0000

    DataReader | BufferredReader - Scanner

--- src/com/weco/JavaResum.java
+++ src/com/weco/JavaResum.java
@@ -258,6 +258,11 @@ NEW.Class - Constructeur - this - Attribut - OBJET/instance
 		System.out.println("For Strings");
 		Stringg strr = new Stringg();
 		strr.showStrings();
+		
+		// For Reader data
+		System.out.println("For Reader data");
+		ReadData read = new ReadData();
+		read.showRead();
 
 	}
 // Class Cat pour instanciation dans le fichier Cat.java
@@ -483,6 +488,77 @@ public class Table {
 
 		sBuffer.insert(0, "Bonjour");
 		System.out.println(sBuffer);
+	    }
+	}
+//-------------------------------------------------------------------------------------
+//	DataReader | BufferredReader - Scanner
+//-------------------------------------------------------------------------------------
+//	- read() - readLine() - skip(N) - nextInt() - nextbyte() - . . .
+	package com.weco;
+
+	import java.io.BufferedReader;
+	import java.io.IOException;
+	import java.io.InputStreamReader;
+	import java.util.Scanner;
+
+	public class ReadData {
+	    public ReadData() throws IOException{
+		
+	    }
+	    
+	    /**
+	     *Show Readers <pre></pre>
+	     *	BufferredReader | 	Scanner <br>
+	     */
+	    void showRead() throws IOException {
+		System.out.println("-----------------------------------");
+		System.out.println("For ReadData");
+		System.out.println("--------------   ----------------");
+		readBufferredReader();
+		readScanner();
+		System.out.println("--------------   ----------------");
+		System.out.println("Merci - Have A good Game ");
+		System.out.println("-----------------------------------");
+
+	    }
+	    
+	    /**
+	     * BufferredReader <pre></pre> 
+	     *		fast for string only  synchrone <br>
+	     *		thread-safe | lecture de String <br>
+	     *		tampon de 8192 caracteres <br>
+	     *		- read() 		: lire un caractere <br> 
+	     *		- readLine() 	: line une chaine <br>
+	     *		- skip(N) 	: ignore (N) carateres 	<br>	
+	     */
+	    void readBufferredReader() throws IOException {
+		System.out.println("--------------BufferredReader");
+
+		InputStreamReader isr = new InputStreamReader(System.in);
+		BufferedReader br = new BufferedReader(isr);
+
+		System.out.println("Comment t'appelles-tu ? ");
+		String name = br.readLine();
+
+		System.out.println("Bonjour " + name);
+	    }
+	    
+	    /**
+	     *	Scanner <pre></pre>
+	     *		check dataType asynchrone <br>
+	     *		not thread-safe <br>
+	     *		lecture de donnee + parsing <br>
+	     *		tampon de 1024 caracteres <br> 
+	     */
+	    void readScanner() {
+		System.out.println("--------------Scanner");
+
+		Scanner sc = new Scanner(System.in);
+
+		System.out.println("Acceder a quel niveau ? ");
+		int game_level = sc.nextInt();
+
+		System.out.println("Niveau " + game_level + "\n" + "Un bon niveau");
 	    }
 	}
 
